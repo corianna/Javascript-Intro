8726413acb4a754093782f06d378dd47166792a5
corianna <annam2004@mail.ru>
2017-07-11 20:20:52 -0500

    Added My first JS

---
+++ scripts/app.js
@@ -0,0 +1,28 @@
+window.onload = function(event) {
+	console.log('Hi mom')
+	console.log(document);
+	let ul = document.createElement('ul');
+	console.log(ul);
+
+
+	document.getElementById('list').innerHTML = 'I like Cats';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
